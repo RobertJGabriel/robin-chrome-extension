3a79be42b9b920060dbf5666ff212f8afe5a9648
Robert Gabriel <rgabriel@mycit.ie>
2015-11-27 21:55:15 +0000

    Added Correct Gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,36 @@
-var gulp = require('gulp'),
-    path = require('path'),
-    concat = require('gulp-concat'),
-    folders = require('gulp-folders'),
-    rjs = require('gulp-requirejs'),
-    clean = require('gulp-clean'),
-    NwBuilder = require('nw-builder'),
-    gutil = require('gulp-util'),
-    say = require('say'),
-    less = require('gulp-less'),
-    minifyCSS = require('gulp-minify-css');
-
-
-
+var gulp = require('gulp');
+var flatten = require('gulp-flatten');
+var less = require('gulp-less');
+var minifyCSS = require('gulp-minify-css');
+var less = require('gulp-less');
+var uglify = require('gulp-uglify');
 
 
 gulp.task('less', function () {
-    gulp.src('./assets/less/style.less')
-        .pipe(less()
-            .on('error', gutil.log)
-            .on('error', gutil.beep)
-            .on('error', function (err) {
-                console.log('err', err);
-                var pathToFile = err.fileName.split('\\');
-                file = pathToFile[pathToFile.length - 1];
-                say.speak('Albert', 'Less is fucked---' + file + '--- Line ' + err.lineNumber);
-            })
-        )
-        .pipe(minifyCSS({
-            keepSpecialComments: 1
-        }))
+    gulp.src('./assets/less/style.less') // path to your file
+        .pipe(less())
         .pipe(gulp.dest('./assets/dist/css/'));
 });
 
 
 gulp.task('popless', function () {
-    gulp.src('./assets/less/popup.less')
-        .pipe(less()
-            .on('error', gutil.log)
-            .on('error', gutil.beep)
-            .on('error', function (err) {
-                console.log('err', err);
-                var pathToFile = err.fileName.split('\\');
-                file = pathToFile[pathToFile.length - 1];
-                say.speak('Albert', 'Less is fucked---' + file + '--- Line ' + err.lineNumber);
-            })
-        )
-        .pipe(minifyCSS({
-            keepSpecialComments: 1
-        }))
+    gulp.src('./assets/less/popup.less') // path to your file
+        .pipe(less())
         .pipe(gulp.dest('./assets/dist/css/'));
 });
 
+gulp.task('fonts', function () {
+    gulp.src('./assets/fonts/*')
+        .pipe(gulp.dest('./assets/dist/fonts'));
+})
+
+
+
+gulp.task('scripts', function () {
+    gulp.src('./assets/js/*/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./assets/dist/js'));
+})
 
 // Default Task
-gulp.task('default', ['less']);
+gulp.task('default', ['less', 'popless', 'scripts', 'fonts']);
